9e3d5650b68e6376760459b45596e763b28b690c
example <example@test.com>
2021-07-24 08:49:05 -0500

    fixed file error

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import logo from "../images/logo.png"
 
 const Header = ({ siteTitle }) => (
   <header
